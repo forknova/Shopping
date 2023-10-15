7d65e53b54df65a471e52d2ca861a2565693468a
forknova <softechstar.jsh@outlook.com>
2023-10-15 18:59:36 +0300

    added login page

--- project/src/app/authentication/login/login.component.ts
+++ project/src/app/authentication/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  constructor(private router: Router, private http: HttpClient) { }
+  message = "";
+  emailbool = true;
+  passwordbool = true;
+  registerForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+  onsubmit() {
+    if (this.registerForm.get('email')?.valid && this.registerForm.get('password')?.valid) {
+      this.http.post("url", this.registerForm.value)
+        .subscribe({
+          next: (bool) => bool ? this.router.navigateByUrl("/homepage") : this.message = "email/password combo is incorrect",
+          error: () => this.message = "Error accured while logging you in"
+        }
+        )
+    }
+  }
+  changeemailbool() {
+    this.registerForm.get('email')?.valid ? this.emailbool = true : this.emailbool = false;
+  }
+  changepasswordbool() {
+    this.registerForm.get('password')?.valid ? this.passwordbool = true : this.passwordbool = false;
+  }
 }
+
--- project/src/app/authentication/register/register.component.ts
+++ project/src/app/authentication/register/register.component.ts
@@ -11,29 +11,25 @@ export class RegisterComponent {
   namebool = true;
   emailbool = true;
   passwordbool = true;
+  message = "";
   registerForm = new FormGroup({
     name: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(8)])
   })
   onsubmit() {
-    console.log(this.registerForm.value)
     if (this.registerForm.get('name')?.valid && this.registerForm.get('email')?.valid && this.registerForm.get('password')?.valid) {
       this.http.post("url", this.registerForm.value)
-        .subscribe(
-          () => {
-
-          },
-          () => {
-            console.log("Error accured");
-          }
+        .subscribe({
+          next: (bool) => bool ? this.message = "account created successfully!" : this.message = "email already registered",
+          error: () => this.message = "Error accured while creating your account"
+        }
         )
     }
   }
   changenamebool() {
     this.registerForm.get('name')?.valid ? this.namebool = true : this.namebool = false;
   }
-
   changeemailbool() {
     this.registerForm.get('email')?.valid ? this.emailbool = true : this.emailbool = false;
   }
