6335fe31c523258c0a0352e4f21e1816fa86525c
forknova <softechstar.jsh@outlook.com>
2024-04-25 12:53:26 +0300

    changin mongodb cluster for testing purposes

--- backend/connection.js
+++ backend/connection.js
@@ -10,8 +10,8 @@ const connection = (async () => {
             throw new Error("MongoDB connection variables not provided");
         }
 
-        const connectionString = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
-
+        //const connectionString = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
+        const connectionString ='mongodb+srv://MainUser:l6ljZurbXpfgTc1k@cluster0.uxgvlqg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
         await mongoose.connect(connectionString, {});
 
         console.log("Connected to MongoDB");
